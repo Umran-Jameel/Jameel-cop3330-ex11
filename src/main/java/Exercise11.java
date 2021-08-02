6d228bca3e9e0138d6ff0ce16979015bdd156319
umranrazack02 <umranrazack02@outlook.com>
2021-08-02 14:34:36 -0400

    challenge dictionary

--- src/main/java/Exercise11.java
+++ src/main/java/Exercise11.java
@@ -8,17 +8,30 @@ import java.util.*;
 public class Exercise11 {
     public static void main(String[] args)
     {
+        Dictionary<String, Double> rates = new Hashtable<>();
+        Dictionary<String, String> currencies = new Hashtable<>();
+        currencies.put("europe", "euros");
+        currencies.put("england", "pounds");
+        currencies.put("mexico", "pesos");
+        currencies.put("canada", "Canadian Dollars");
+        rates.put("euros", 1.19);
+        rates.put("pounds", 1.39);
+        rates.put("pesos", 0.050);
+        rates.put("Canadian Dollars", 0.80);
+
         Scanner in = new Scanner(System.in);
 
-        System.out.print("How many euros are you exchanging? ");
-        String eur = in.nextLine();
-        int euros = Integer.parseInt(eur);
+        System.out.print("Which country (Europe, England, Mexico, Canada)? ");
+        String country = in.nextLine().toLowerCase();
+        String currency = currencies.get(country);
+        double rate = rates.get(currency);
 
-        System.out.print("What is the exchange rate? ");
-        String exch = in.nextLine();
-        double rate = Double.parseDouble(exch);
+        System.out.print("How many " + currency + " are you exchanging: ");
+        String amount = in.nextLine();
+        double quantity = Double.parseDouble(amount);
 
-        System.out.printf("%d euros at an exchange rate of %f is\n%.2f U.S. Dollars.", euros, rate, euros * rate);
+
+        System.out.printf("%.2f %s at an exchange rate of %f is\n%.2f U.S. Dollars.", quantity, currency, rate, quantity * rate);
 
     }
 }
